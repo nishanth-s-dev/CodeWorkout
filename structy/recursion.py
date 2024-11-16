85c3d399a69703fa443cff780d25d0b0d49dacd7
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-11-16 17:02:26 +0530

    recursive problems added

--- structy/recursion.py
+++ structy/recursion.py
@@ -3,3 +3,21 @@ def sum_numbers_recursive(numbers):
   if not numbers:
     return 0
   return numbers[0] + sum_numbers_recursive(numbers[1:])
+
+# https://www.structy.net/problems/premium/factorial
+def factorial(n):
+  if n == 0:
+    return 1
+  return n * factorial(n - 1)
+
+# https://www.structy.net/problems/premium/sum-of-lengths
+def sum_of_lengths(strings):
+  if not strings:
+    return 0
+  return len(strings[0]) + sum_of_lengths(strings[1:])
+
+# https://www.structy.net/problems/premium/reverse-string-recursive
+def reverse_string(s):
+  if not s:
+    return ''
+  return reverse_string(s[1:]) + s[0]
