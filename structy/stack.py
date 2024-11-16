b2118930c517c7316a4b2e45fa05ba7dd07d3f3f
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-11-16 16:45:59 +0530

    nesting score stack problem added

--- structy/stack.py
+++ structy/stack.py
@@ -45,4 +45,19 @@ def decompress_braces(string):
       stack.append(c)
   return "".join(stack)
 
-print(decompress_braces("2{q}3{tu}v"))
+
+def nesting_score(string):
+  stack = [0]
+  for c in string:
+    if c == "[":
+      stack.append(0)
+    else:
+      top = stack.pop()
+      if top == 0:
+        stack[-1] += 1
+      else:
+        stack[-1] += 2 * top
+  return stack.pop()
+
+
+nesting_score("[]")
