eba57e97d9f391ebfea0425722691859120c242d
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2024-11-18 01:44:02 +0530

    remove_node problem added [recursive]

--- python/linked_list/remove_node.py
+++ python/linked_list/remove_node.py
@@ -17,3 +17,18 @@ def remove_node(head, target_val):
     prev = current
     current = current.next
   return head
+
+def remove_node(head, target_val):
+  if head.val == target_val:
+    return head.next
+
+  current = head
+  prev = None
+
+  while current is not None:
+    if current.val == target_val:
+      prev.next = current.next
+      break
+    prev = current
+    current = current.next
+  return head
